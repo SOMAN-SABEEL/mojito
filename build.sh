ff5cfef46599e65c54e7cdfc4b64e004e351433a
Soman Sabeel <84325440+SOMAN-SABEEL@users.noreply.github.com>
2025-02-23 13:20:46 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-# Set up environment 
+# Set up environment
+echo "======= SettingUp Build Enviroment ======"
 . build/envsetup.sh
-
+echo "======= Build Enviroment Setup Complete ======"
 # Choose a target device 
 lunch cipher_mojito-ap1a-userdebug
-
+echo "======= Settting Up Device Complete======"
 # Compile CipherOS 
+echo "======= Compiling ROM ======"
 mka bacon -j$(nproc --all)
+echo "======= Done ======"
