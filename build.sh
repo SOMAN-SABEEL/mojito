d13f49fbd724eaa8158dcf7e136ba6d3da7d7035
Soman Sabeel <84325440+SOMAN-SABEEL@users.noreply.github.com>
2025-02-24 15:09:39 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,15 +1,47 @@
 #!/bin/bash
 
-# Set up environment
-echo "======= SettingUp Build Enviroment ======"
-. build/envsetup.sh
-echo "======= Build Enviroment Setup Complete ======"
-# Choose a target device 
-lunch mojito-ap1a-userdebug
-echo "======= Settting Up Device Complete======"
-# Compile CipherOS 
+rm -rf .repo/local_manifests/
+
+# Local TimeZone
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/India /etc/localtime
+
+# repo init rom
+repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs
+echo "=================="
+echo "Repo init success"
+echo "=================="
+
+# Local manifests
+git clone -b Crd-15 https://github.com/Sachinpawar86/local_manifests .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+
+# Custom Source
+wget https://raw.githubusercontent.com/custom-crdroid/custom_cr_setup/refs/heads/15.0/vendorsetup.sh
+bash vendorsetup.sh
+
+# Sync the repositories
+/opt/crave/resync.sh
+echo "============================"
+
+# Export
+export BUILD_USERNAME=Soman_Sabeel
+export BUILD_HOSTNAME=crave
+echo "======= Export Done ======"
+
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
+
+# Lunch
+lunch lineage_mojito-ap3a-user
+echo "============="
+
 # Make cleaninstall
 make installclean
-echo "======= Compiling ROOOOM ======"
-mka bacon -j$(nproc --all)
-echo "======= Doneeee ======"
+echo "============="
+
+# Build rom
+mka bacon
