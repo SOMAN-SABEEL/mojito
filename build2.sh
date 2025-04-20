414ba640a20cf5c54f232049fff38530b0b24cad
Soman Sabeel <84325440+SOMAN-SABEEL@users.noreply.github.com>
2025-04-20 13:55:04 +0530

    Update build2.sh

--- build2.sh
+++ build2.sh
@@ -3,14 +3,10 @@
 export BUILD_USERNAME=Soman_Sabeel
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
-
-# Set up build environment
-. build/envsetup.sh 
-echo "====== Envsetup Done ======="
-
-# Lunch
-riseup mojito userdebug
-echo "==== brunching ====="
-
-# Build rom
-rise b
+# env Setup
+. build/envsetup.sh
+ # Lunch 
+lunch riseup_mojito-userdebug
+# baking
+mka bacon
+# Done
