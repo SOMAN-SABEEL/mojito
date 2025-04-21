698ef4897311a0623c919b6e8edc0e60807aa9ba
Soman Sabeel <84325440+SOMAN-SABEEL@users.noreply.github.com>
2025-04-21 13:29:02 +0530

    Update build2.sh

--- build2.sh
+++ build2.sh
@@ -2,11 +2,21 @@
 # Export
 export BUILD_USERNAME=Soman_Sabeel
 export BUILD_HOSTNAME=crave
-echo "======= Export Done ======"
-# env Setup
+echo "===== Export Done ======"
+# updating 
+sudo apt update && sudo apt install ccache -y
+echo "=== Update Done ==="
+# Ccache error fix
+export USE_CCACHE=1
+ccache -M 50G
+echo "=== ccache error fix done"
+# Build setup
 . build/envsetup.sh
- # Lunch 
-riseup mojito userdebug
-# baking
-rise b
-# Done
+echo "==== Build Setup Done ===="
+# lunch
+lunch riseup_mojito-userdebug
+# Baking
+echo "=== Started Baking ==="
+mka bacon
+echo "=== Done ==="
+
