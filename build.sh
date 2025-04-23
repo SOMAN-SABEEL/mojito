6df4542d02afb0071ea348007b785c4806204db6
Soman Sabeel <84325440+SOMAN-SABEEL@users.noreply.github.com>
2025-04-23 13:22:13 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -7,15 +7,13 @@ sudo rm -rf /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Asia/India /etc/localtime
 
 # Rom source repo
-repo init -u https://github.com/CipherOS/android_manifest.git -b fifteen
-# Sync 
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo init -u https://github.com/Evolution-X/manifest -b vic --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
 
 # Clone local_manifests repository
-git clone https://github.com/SOMAN-SABEEL/mojito/local_manifest.xml .repo/local_manifests
+git clone -b Evo-15-QPR2 https://github.com/Sachinpawar86/local_manifests .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
@@ -30,11 +28,17 @@ export BUILD_USERNAME=Soman_Sabeel
 export BUILD_HOSTNAME=crave
 echo "======= Export Done ======"
 
-# Set up environment 
-. build/envsetup.sh
+# Set up build environment
+source build/envsetup.sh
+echo "====== Envsetup Done ======="
 
-# Choose a target device 
-lunch cipher_mojito-ap3a-userdebug
+# Lunch
+lunch lineage_mojito-bp1a-user
+echo "============="
 
-# Compile CipherOS 
-mka bacon -j$(nproc --all)
+# Make cleaninstall
+make installclean
+echo "============="
+
+# Build rom
+m evolution
