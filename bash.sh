6e9bbf4c95d005efdda10339f745601c6cb6c8a9
Soman Sabeel <84325440+SOMAN-SABEEL@users.noreply.github.com>
2025-06-21 12:52:56 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,26 +1,27 @@
 #!/bin/bash
 set -e
 
-# KernelSU-Next setup
+# Setup KernelSU-Next
 curl -LSs "https://raw.githubusercontent.com/KernelSU-Next/KernelSU-Next/next/kernel/setup.sh" | bash -
 
-# Export environment
+# Export build environment
 export ARCH=arm64
 export SUBARCH=arm64
 export KBUILD_BUILD_USER=Soman
 export KBUILD_BUILD_HOST=crave
 
+# Proton Clang path 
 export CLANG_PATH=$(pwd)/proton-clang
 export PATH=$CLANG_PATH/bin:$PATH
 
-#clean output dir for a fresh build
+# Clean previous output
 rm -rf out
 
-# Build defconfig
+# Load default config
 make O=out mojito_defconfig
 
-# Compile kernel
+# Build kernel
 make -j$(nproc) O=out \
     CC=clang \
-    CROSS_COMPILE=aarch64-linux-gnu- \
-    CROSS_COMPILE_ARM32=arm-linux-gnueabi-
+    CROSS_COMPILE=${CLANG_PATH}/bin/aarch64-linux-gnu- \
+    CROSS_COMPILE_ARM32=${CLANG_PATH}/bin/arm-linux-gnueabi-
