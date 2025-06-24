5ac04a8c859ab9905f73d069fd30374e923b49d0
Soman Sabeel <84325440+SOMAN-SABEEL@users.noreply.github.com>
2025-06-24 21:53:54 +0530

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,10 +1,25 @@
 #!/bin/bash
 
+# Initialize repo
+repo init -u https://github.com/CipherOS/android_manifest.git -b fifteen
+
 # Sync source (safe)
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
 
+# Clone device resources
+git clone --depth=1 -b mojito-voyager https://github.com/SOMAN-SABEEL/android_device_xiaomi_sm6150-common.git device/xiaomi/sm6150-common
+git clone --depth=1 -b mojito-voyager https://github.com/SOMAN-SABEEL/device_xiaomi_mojito.git device/xiaomi/mojito
+git clone --depth=1 -b voyager https://github.com/SOMAN-SABEEL/android_hardware_xiaomi.git hardware/xiaomi
+git clone --depth=1 -b inline-rom https://github.com/SOMAN-SABEEL/kernel_xiaomi_mojito.git kernel/xiaomi/mojito
+git clone --depth=1 -b 15 https://gitlab.com/romgharti/android_vendor_xiaomi_mojito.git vendor/xiaomi/mojito
+git clone --depth=1 -b 15 https://gitlab.com/romgharti/android_vendor_xiaomi_sm6150-common.git vendor/xiaomi/sm6150-common
+
 # Set up environment
 . build/envsetup.sh
+
+# Export build flags
+export CIPHER_MAINTAINER="Soman Sabeel"
+export CIPHER_GAPPS=true
 
 # Lunch device
 lunch cipher_mojito-ap1a-userdebug
