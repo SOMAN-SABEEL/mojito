e10e3f246a50b51c5b0f74a0160202bcb88ce7a3
Soman Sabeel <84325440+SOMAN-SABEEL@users.noreply.github.com>
2025-09-14 18:37:38 +0530

    Update build2.sh

--- build2.sh
+++ build2.sh
@@ -1,45 +1,30 @@
 #!/bin/bash
-
 # Remove old local_manifests
-rm -rf .repo/local_manifests/
-
-# Local TimeZone
-sudo rm -rf /etc/localtime
-
 # ROM source repo
 repo init -u https://github.com/LineageOS/android.git -b lineage-22.2 --git-lfs
 echo "=================="
 echo "Repo init success"
 echo "=================="
-
 # Clone local_manifests repository
- clone https://github.com/SOMAN-SABEEL/local_manifest .repo/local_manifests
 echo "============================"
 echo "Local manifest clone success"
 echo "============================"
-
 # Signed Keys
-
 # Sync the repositories
 /opt/crave/resync.sh
 echo "============================"
-
 # Export
 export BUILD_USERNAME="Soman"
 export BUILD_HOSTNAME="crave"
 echo "======= Export Done ======"
-
 # Set up build environment
 source build/envsetup.sh
 echo "====== Envsetup Done ======="
-
 # Lunch
-lunch lineage_mojito-userdebug
+lunch lineage_mojito-ap2a-userdebug
 echo "============="
-
 # Make clean install
 make installclean
 echo "============="
-
 # Build ROM
 m bacon
