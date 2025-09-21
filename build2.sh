6d6b49aafdc119c3226e7d352d3879454ab59f22
Soman Sabeel <84325440+SOMAN-SABEEL@users.noreply.github.com>
2025-09-21 14:43:11 +0530

    Update build2.sh

--- build2.sh
+++ build2.sh
@@ -1,4 +1,25 @@
 #!/bin/bash
+# Remove old local_manifests
+rm -rf .repo/local_manifests/
+
+# Local TimeZone
+sudo rm -rf /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Asia/Kolkata /etc/localtime
+
+# ROM source repo
+repo init -u https://github.com/Evolution-X/manifest -b bka --git-lfs
+# Clone local_manifests repository
+git clone -b Evo-16 https://github.com/Sachinpawar86/local_manifests .repo/local_manifests
+echo "============================"
+echo "Local manifest clone success"
+echo "============================"
+# Signed Keys
+crave ssh
+git clone https://github.com/Evolution-X/vendor_evolution-priv_keys-template vendor/evolution-priv/keys
+cd vendor/evolution-priv/keys || exit 1
+./keys.sh
+cd ../../..
+echo "===== Keys setup done ====="
 echo "================ sync =============="
 /opt/crave/resync.sh
 echo "============================"
